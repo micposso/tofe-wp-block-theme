faa438fa279eca15d11a5c01003016c075a3ad2e
micposso <micposso@gmail.com>
2024-01-29 23:40:37 -0500

    first commit

---
+++ patterns/page.php
@@ -0,0 +1,183 @@
+<?php
+/**
+ * Title: page
+ * Slug: tove/page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header","align":"full","className":"site-header"} /-->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|70","padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"default"}} -->
+<main class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|70"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:media-text {"align":"wide","mediaPosition":"right","mediaLink":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/cup-illustration-square.png","mediaType":"image","mediaSizeSlug":"full","backgroundColor":"secondary"} -->
+<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile has-secondary-background-color has-background"><div class="wp-block-media-text__content"><!-- wp:group {"layout":{"inherit":false,"contentSize":"500px","type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">Comfort in a Cup</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">Feeling a bit down about summer ending? Our Pumpkin Spice Latte will help you get excited for the season of the witch.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Read More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div><figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/cup-illustration-square.png" alt=""/></figure></div>
+<!-- /wp:media-text -->
+
+<!-- wp:columns {"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:cover {"customOverlayColor":"#f6f6f6","minHeight":600,"className":"is-light"} -->
+<div class="wp-block-cover is-light" style="min-height:600px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#f6f6f6"></span><div class="wp-block-cover__inner-container"><!-- wp:image {"align":"center","id":571,"sizeSlug":"medium","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-medium"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/wire-sculpture-263x300.jpg" alt="" class="wp-image-571"/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"1em","right":"1em","bottom":"1em","left":"1em"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:1em;padding-right:1em;padding-bottom:1em;padding-left:1em"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"320px"} -->
+<div class="wp-block-column" style="flex-basis:320px"><!-- wp:heading -->
+<h2 class="wp-block-heading"><strong>Airplane</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Copper wire, wood base. I created this piece in late 2008. For this work, I aimed to convey both the industrial heaviness of an airplane, but also the cloudlike floating quality you feel when you’re in one.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"1em","right":"1em","bottom":"1em","left":"1em"}}},"layout":{"type":"constrained","justifyContent":"right"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:1em;padding-right:1em;padding-bottom:1em;padding-left:1em"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"320px"} -->
+<div class="wp-block-column" style="flex-basis:320px"><!-- wp:heading -->
+<h2 class="wp-block-heading"><strong>Airplane</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Copper wire, wood base. I created this piece in late 2008. For this work, I aimed to convey both the industrial heaviness of an airplane, but also the cloudlike floating quality you feel when you’re in one.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:cover {"customOverlayColor":"#f6f6f6","minHeight":600,"className":"is-light"} -->
+<div class="wp-block-cover is-light" style="min-height:600px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#f6f6f6"></span><div class="wp-block-cover__inner-container"><!-- wp:image {"align":"center","id":571,"sizeSlug":"medium","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-medium"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/wire-sculpture-263x300.jpg" alt="" class="wp-image-571"/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"backgroundColor":"senary"} -->
+<div class="wp-block-column has-senary-background-color has-background"><!-- wp:group {"backgroundColor":"senary"} -->
+<div class="wp-block-group has-senary-background-color has-background"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none","className":"is-resized is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/avatar-3.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:quote {"align":"center","className":"is-style-default"} -->
+<blockquote class="wp-block-quote has-text-align-center is-style-default"><!-- wp:paragraph -->
+<p>“Niofika Café serves the best coffee in Stockholm. It’s not even close.”</p>
+<!-- /wp:paragraph --><cite>Coffee Snob</cite></blockquote>
+<!-- /wp:quote --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"backgroundColor":"senary"} -->
+<div class="wp-block-column has-senary-background-color has-background"><!-- wp:group {"backgroundColor":"senary"} -->
+<div class="wp-block-group has-senary-background-color has-background"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none","className":"is-resized is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/avatar-1.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:quote {"align":"center","className":"is-style-default"} -->
+<blockquote class="wp-block-quote has-text-align-center is-style-default"><!-- wp:paragraph -->
+<p>“Niofika Café serves the best coffee in Stockholm. It’s not even close.”</p>
+<!-- /wp:paragraph --><cite>Coffee Snob</cite></blockquote>
+<!-- /wp:quote --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"backgroundColor":"senary"} -->
+<div class="wp-block-column has-senary-background-color has-background"><!-- wp:group {"backgroundColor":"senary"} -->
+<div class="wp-block-group has-senary-background-color has-background"><!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none","className":"is-resized is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-full is-resized is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/avatar-2.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:quote {"align":"center","className":"is-style-default"} -->
+<blockquote class="wp-block-quote has-text-align-center is-style-default"><!-- wp:paragraph -->
+<p>“Niofika Café serves the best coffee in Stockholm. It’s not even close.”</p>
+<!-- /wp:paragraph --><cite>Coffee Snob</cite></blockquote>
+<!-- /wp:quote --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"wide","backgroundColor":"senary","className":"is-style-tove-shaded"} -->
+<div class="wp-block-group alignwide is-style-tove-shaded has-senary-background-color has-background"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":3} -->
+<h3 class="wp-block-heading has-text-align-center">Be Our Guest!</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Book a table and experience our award-winning seasonal menu with meat and
+			vegetables sourced from local farms!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--40)"><!-- wp:button {"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Make a
+					Reservation</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"0","bottom":"var:preset|spacing|80","left":"0"}}},"backgroundColor":"white","layout":{"type":"default"}} -->
+<div class="wp-block-group has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:site-title {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"medium"} /-->
+
+<!-- wp:site-tagline {"textAlign":"center"} /-->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button">Watch my latest videos</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button">Buy merch</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button">Support me on Patreon</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"width":100,"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link wp-element-button">Get tickets for my show</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:social-links {"iconColor":"black","iconColorValue":"#000000","iconBackgroundColor":"white","iconBackgroundColorValue":"#ffffff","size":"has-normal-icon-size","style":{"spacing":{"blockGap":{"top":"0","left":"0"},"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
+<ul class="wp-block-social-links has-normal-icon-size has-icon-color has-icon-background-color" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:social-link {"url":"https://wordpress.org/patterns/","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"https://wordpress.org/patterns/","service":"bandcamp"} /-->
+
+<!-- wp:social-link {"url":"https://wordpress.org/patterns/","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"https://wordpress.org/patterns/","service":"twitch"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","align":"full","className":"site-footer no-margin"} /-->
